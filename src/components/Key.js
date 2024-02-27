c358e13664785654846fdbb2f90585499a287033
Daniel at CosmicDNA <daniel@cosmicdna.co.uk>
2024-02-27 18:15:06 +0000

    :alembic: Test a raycast per key.

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,12 +2,23 @@ import PropTypes from 'prop-types'
 import React, { useState, useRef } from 'react'
 import * as THREE from 'three'
 import { useDrag } from '@use-gesture/react'
-import { useCycleRaycastContext } from '../hooks/useCycleRaycast'
+// import { CycleRaycast } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
+// import { useCycleRaycastContext } from '../hooks/useCycleRaycast'
+
+// import { useCycleRaycastContext } from '../hooks/useCycleRaycast'
+
+// const { setCycleRayCastState, CycleRaycast } = useCycleRaycastContext()
+// <CycleRaycast onChanged={setCycleRayCastState} />
 
 const Key = ({ roundResolution = 32, width = 8 / 10, lateral = 7 / 10, depth = 1 / 20, keyId, round, ...props }) => {
   const meshRef = useRef()
   const [pressed, setPressed] = useState(false)
-  const { cycleRayCastState } = useCycleRaycastContext()
+  // const [cycleRayCastState, setCycleRayCastState] = useState({
+  //   objects: [],
+  //   cycle: 0
+  // })
+  const { raycaster, camera } = useThree()
   const widthOnTwo = width / 2
 
   let pts
@@ -44,10 +55,20 @@ const Key = ({ roundResolution = 32, width = 8 / 10, lateral = 7 / 10, depth = 1
     setPressed(false)
   }
 
-  const bind = useDrag(({ down }) => {
+  const bind = useDrag(({ event, down }) => {
+    // const { down } = event
+    const { clientX, clientY } = event
     if (down && pressed) {
+      // Assuming you have clientX and clientY from the event
+      const coords = new THREE.Vector2()
+      coords.x = (clientX / window.innerWidth) * 2 - 1
+      coords.y = -(clientY / window.innerHeight) * 2 + 1
+      raycaster.setFromCamera(coords, camera)
+      const intersects = raycaster.intersectObject(meshRef.current)
+      // console.log(intersects)
       // If the pressed key is no longer found in list of pierced through objects, release it.
-      if (!cycleRayCastState.find(obj => obj?.object?.uuid === meshRef.current.uuid)) {
+      if (!intersects.find(obj => obj?.object?.uuid === meshRef.current.uuid)) {
+        console.log(intersects)
         onPointerUp()
       }
     }
@@ -76,6 +97,7 @@ const Key = ({ roundResolution = 32, width = 8 / 10, lateral = 7 / 10, depth = 1
           {/* eslint-disable-next-line react/no-unknown-property */}
           <extrudeGeometry args={[new THREE.Shape(pts.map(points => new THREE.Vector2(...points))), extrudeSettings]} />
         </mesh>
+        {/* <CycleRaycast onChanged={setCycleRayCastState} /> */}
     </group>
   )
 }
--- src/components/StenoKeyboard.js
+++ src/components/StenoKeyboard.js
@@ -1,30 +1,24 @@
 import React from 'react'
 import KeyGroup from './KeyGroup'
 import Key from './Key'
-import { useCycleRaycastContext } from '../hooks/useCycleRaycast'
 
 const StenoKeyboard = (props) => {
   const enter = 0.2
   const p = 1.3
 
-  const { setCycleRayCastState, CycleRaycast } = useCycleRaycastContext()
-
   return (
-    <>
-      {/* eslint-disable-next-line react/no-unknown-property */}
-      <group {...props} rotation-x={-Math.PI / 2}>
-        <Key keyId={'#'} width={9.8} position={[-0.5, 4.8, 0]}/>
-        <Key keyId={'*'} round={true} lateral={2} position={[-1, 3 * p, 0]}/>
-        <Key keyId={'S-'} round={true} lateral={2} position={[-5, 3 * p, 0]}/>
-        <KeyGroup keys={['-F', '-P', '-L', '-T', '-D']} name={'a'} position={[0, 3 * p, 0]} lateral={1.1}/>
-        <KeyGroup keys={['-R', '-G', '-B', '-S', '-Z']} name={'b'} position={[0, 2 * p, 0]} round={true}/>
-        <KeyGroup keys={['A', 'O']} name={'c'} position={[0 - enter, p, 0]} round={true}/>
-        <KeyGroup keys={['E', 'U']} name={'d'} position={[-3 + enter, p, 0]} round={true}/>
-        <KeyGroup keys={['T-', 'P-', 'H-']} name={'e'} position={[-4, 3 * p, 0]} lateral={1.1}/>
-        <KeyGroup keys={['K-', 'W-', 'R-']} name={'f'} position={[-4, 2 * p, 0]} round={true}/>
-      </group>
-      <CycleRaycast onChanged={setCycleRayCastState} />
-    </>
+    // eslint-disable-next-line react/no-unknown-property
+    <group {...props} rotation-x={-Math.PI / 2}>
+      <Key keyId={'#'} width={9.8} position={[-0.5, 4.8, 0]}/>
+      <Key keyId={'*'} round={true} lateral={2} position={[-1, 3 * p, 0]}/>
+      <Key keyId={'S-'} round={true} lateral={2} position={[-5, 3 * p, 0]}/>
+      <KeyGroup keys={['-F', '-P', '-L', '-T', '-D']} name={'a'} position={[0, 3 * p, 0]} lateral={1.1}/>
+      <KeyGroup keys={['-R', '-G', '-B', '-S', '-Z']} name={'b'} position={[0, 2 * p, 0]} round={true}/>
+      <KeyGroup keys={['A', 'O']} name={'c'} position={[0 - enter, p, 0]} round={true}/>
+      <KeyGroup keys={['E', 'U']} name={'d'} position={[-3 + enter, p, 0]} round={true}/>
+      <KeyGroup keys={['T-', 'P-', 'H-']} name={'e'} position={[-4, 3 * p, 0]} lateral={1.1}/>
+      <KeyGroup keys={['K-', 'W-', 'R-']} name={'f'} position={[-4, 2 * p, 0]} round={true}/>
+    </group>
   )
 }
 
