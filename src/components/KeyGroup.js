b0d299ece07f67acab595afae8b776740e67a585
Daniel at CosmicDNA <daniel@cosmicdna.co.uk>
2024-02-27 19:25:38 +0000

    :art: Organise StenoKeyboard.js

--- src/components/KeyGroup.js
+++ src/components/KeyGroup.js
@@ -3,13 +3,10 @@ import PropTypes from 'prop-types'
 import Key from './Key'
 
 const KeyGroup = ({ keys, name, position, round, ...props }) => {
-  const x = position[0]
-  const y = position[1]
-  const z = position[2]
   return (
     <>
       {[...Array(keys.length).keys()].map(i =>
-        <Key position={[x + i, y, z]} keyId={keys[i]} key={`${name}_${keys[i]}`} round={round} {...props}/>
+        <Key position={[position.x + i, position.y, position.z]} keyId={keys[i]} key={`${name}_${keys[i]}`} round={round} {...props}/>
       )}
     </>
   )
--- src/components/StenoKeyboard.js
+++ src/components/StenoKeyboard.js
@@ -1,23 +1,60 @@
 import React from 'react'
 import KeyGroup from './KeyGroup'
 import Key from './Key'
+import * as THREE from 'three'
+
+const enter = 0.2
+const rowSpacing = 1.3
+
+const referencePosition = new THREE.Vector3(0.5, -2.4, 0)
+const position = [
+  [-0.5, 4.8, 0],
+  [-1, 3 * rowSpacing, 0],
+  [-5, 3 * rowSpacing, 0],
+  [0, 3 * rowSpacing, 0],
+  [0, 2 * rowSpacing, 0],
+  [-enter, rowSpacing, 0],
+  [-3 + enter, rowSpacing, 0],
+  [-4, 3 * rowSpacing, 0],
+  [-4, 2 * rowSpacing, 0]
+]
+  .map(p => (new THREE.Vector3(...p))
+    .add(referencePosition))
+
+const round = true
+const config = [
+  { type: 'Key', keyId: '#', width: 9.8, position: position[0] },
+  { type: 'Key', keyId: '*', lateral: 2, position: position[1], round },
+  { type: 'Key', keyId: 'S-', lateral: 2, position: position[2], round },
+  { type: 'Row', keys: ['-F', '-P', '-L', '-T', '-D'], position: position[3], lateral: 1.1 },
+  { type: 'Row', keys: ['-R', '-G', '-B', '-S', '-Z'], position: position[4], round },
+  { type: 'Row', keys: ['A', 'O'], position: position[5], round },
+  { type: 'Row', keys: ['E', 'U'], position: position[6], round },
+  { type: 'Row', keys: ['T-', 'P-', 'H-'], position: position[7], lateral: 1.1 },
+  { type: 'Row', keys: ['K-', 'W-', 'R-'], position: position[8], round }
+]
+
+const rowItems = config.filter(o => o.type === 'Row')
 
 const StenoKeyboard = (props) => {
-  const enter = 0.2
-  const p = 1.3
-
   return (
     // eslint-disable-next-line react/no-unknown-property
     <group {...props} rotation-x={-Math.PI / 2}>
-      <Key keyId={'#'} width={9.8} position={[-0.5, 4.8, 0]}/>
-      <Key keyId={'*'} round={true} lateral={2} position={[-1, 3 * p, 0]}/>
-      <Key keyId={'S-'} round={true} lateral={2} position={[-5, 3 * p, 0]}/>
-      <KeyGroup keys={['-F', '-P', '-L', '-T', '-D']} name={'a'} position={[0, 3 * p, 0]} lateral={1.1}/>
-      <KeyGroup keys={['-R', '-G', '-B', '-S', '-Z']} name={'b'} position={[0, 2 * p, 0]} round={true}/>
-      <KeyGroup keys={['A', 'O']} name={'c'} position={[0 - enter, p, 0]} round={true}/>
-      <KeyGroup keys={['E', 'U']} name={'d'} position={[-3 + enter, p, 0]} round={true}/>
-      <KeyGroup keys={['T-', 'P-', 'H-']} name={'e'} position={[-4, 3 * p, 0]} lateral={1.1}/>
-      <KeyGroup keys={['K-', 'W-', 'R-']} name={'f'} position={[-4, 2 * p, 0]} round={true}/>
+      {
+        config.map((item, index) => {
+          let rowIndex
+          switch (item.type) {
+            case 'Key':
+              return <Key {...item} />
+            case 'Row':
+              rowIndex = rowItems
+                .findIndex(o => JSON.stringify(o) === JSON.stringify(item))
+              return <KeyGroup {...{ ...item, name: `g${rowIndex}` }} />
+            default:
+              return null
+          }
+        })
+      }
     </group>
   )
 }
