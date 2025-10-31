e384c1f95cf68d3b9bc4bc94b827da86b208b014
DanielAtCosmicDNA <daniel@cosmicdna.co.uk>
2025-10-31 23:12:50 +0000

    :heavy_plus_sign: Add pause to useStrokeEvents.

--- src/components/hooks/use-stroke-events.js
+++ src/components/hooks/use-stroke-events.js
@@ -1,18 +1,22 @@
 import usePeriodicFrame from './use-periodic-frame'
 import { useMemo, useRef } from 'react'
 
-const resolution = 10
-
-const useStrokeEvents = (straightForwardStrokes, { ratio = 6, enabled, nextObjectCallback = () => {}, nextStrokeCallback = () => {}, nextStrokeCallbackDT = () => {} }) => {
+const useStrokeEvents = (straightForwardStrokes, { ratio = 6, pauseSeconds = 3, resolution = 10, enabled, nextObjectCallback = () => {}, nextStrokeCallback = () => {}, nextStrokeCallbackDT = () => {} }) => {
   const { allStenoKeys, cumulativeStrokeLengths } = useMemo(() => {
-    const allStenoKeys = straightForwardStrokes.flatMap(o => o.steno)
+    if (!enabled) return { allStenoKeys: [], cumulativeStrokeLengths: [] }
+    const originalStenoKeys = straightForwardStrokes.flatMap(o => o.steno)
+
+    // Pad the array with nulls to create a pause at the end of the sequence.
+    const padding = Array(pauseSeconds).fill(null)
+    const allStenoKeys = [...originalStenoKeys, ...padding]
+
     let cumulativeLength = 0
     const cumulativeStrokeLengths = straightForwardStrokes.map(stroke => {
       cumulativeLength += stroke.steno.length
       return cumulativeLength
     })
     return { allStenoKeys, cumulativeStrokeLengths }
-  }, [straightForwardStrokes])
+  }, [enabled, straightForwardStrokes, pauseSeconds])
 
   const lastAnimatedStrokeIndex = useRef(null)
 
@@ -25,22 +29,23 @@ const useStrokeEvents = (straightForwardStrokes, { ratio = 6, enabled, nextObjec
       }
       if (indexer % resolution === 0) {
         const index = indexer / resolution
-
         const iterator = index % allStenoKeys.length
+        const currentKey = allStenoKeys[iterator]
 
-        const strokeIndex = cumulativeStrokeLengths.findIndex(len => iterator < len)
+        if (currentKey !== null) {
+          // Not in pause...
+          const strokeIndex = cumulativeStrokeLengths.findIndex(len => iterator < len)
 
-        // Check if the stroke object has changed since the last frame
-        if (strokeIndex !== lastAnimatedStrokeIndex.current) {
-          const strokeObject = straightForwardStrokes[strokeIndex]
-          // Event: The stroke object has changed.
-          // console.log('Next stroke:', strokeObject)
-          nextObjectCallback(strokeObject)
-          // Update the ref to the new index for the next check.
-          lastAnimatedStrokeIndex.current = strokeIndex
+          // Check if the stroke object has changed since the last frame
+          if (strokeIndex !== lastAnimatedStrokeIndex.current) {
+            const strokeObject = straightForwardStrokes[strokeIndex]
+            // Event: The stroke object has changed.
+            nextObjectCallback(strokeObject)
+            // Update the ref to the new index for the next check.
+            lastAnimatedStrokeIndex.current = strokeIndex
+          }
+          nextStrokeCallback(currentKey, timeReference)
         }
-        // console.log(allStenoKeys[iterator])
-        nextStrokeCallback(allStenoKeys[iterator], timeReference)
       }
     }
   }, { enabled })
